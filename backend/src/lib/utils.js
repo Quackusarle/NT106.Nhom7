d25fb357a8c4c8e30e6ebe637219b77f884e627d
Quackusarle <tvhoangg05@gmail.com>
2025-04-15 12:31:52 +0700

    update cloudinary.js and utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -5,4 +5,15 @@ dotenv.config();
 
 export const generateToken = (userID, res) => {
     const token = jwt.sign({userID}, process.env.JWT_SECRET, {expiresIn: '30d'});
+
+
+    res.cookie("jwt", token, {
+        maxAge: 30*24*60*60*1000,
+        httpOnly: true,
+        sameSite:"strict",
+        secure: process.env.NODE_ENV !== 'development' 
+    }); 
+
+    return token;
+
 }
